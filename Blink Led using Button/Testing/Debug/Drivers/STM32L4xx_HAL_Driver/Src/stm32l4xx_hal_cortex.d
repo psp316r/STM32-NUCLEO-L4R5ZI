862672a08e27b007f9bb994576f4e0d1df9a04b2
psp316r <preetham.p1997@gmail.com>
2020-04-16 15:17:46 +0530

    Blink Led

---
+++ Blink Led using Button/Testing/Debug/Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.d
@@ -0,0 +1,104 @@
+Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.o: \
+ ../Drivers/STM32L4xx_HAL_Driver/Src/stm32l4xx_hal_cortex.c \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h \
+ /home/psp/Desktop/Testing/Inc/stm32l4xx_hal_conf.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4r5xx.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Include/core_cm4.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_version.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_compiler.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Include/mpu_armv7.h \
+ /home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pcd.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usb.h \
+ /home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pcd_ex.h
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
+
+/home/psp/Desktop/Testing/Inc/stm32l4xx_hal_conf.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_def.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/stm32l4r5xx.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_version.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_compiler.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Include/mpu_armv7.h:
+
+/home/psp/Desktop/Testing/Drivers/CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_rcc_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_exti.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_gpio_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_dma_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_cortex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_flash_ramfunc.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_i2c_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pcd.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_ll_usb.h:
+
+/home/psp/Desktop/Testing/Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pcd_ex.h:
